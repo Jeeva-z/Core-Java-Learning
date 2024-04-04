11fc4c3a287c3c8fa407163f9a90f09be653e4c4
JEEVA S U <116629402+Jeeva-z@users.noreply.github.com>
2024-04-04 21:47:52 +0530

    Update TransferException.java

--- ExceptionHandling/TransferException.java
+++ ExceptionHandling/TransferException.java
@@ -1,11 +1,21 @@
 package org.java;
 
 public class TransferException extends Exception{
+    String message;
+    
     TransferException(String message){
         super(message);
+        this.message = message;
     }
 
-    TransferException(){
+    TransferException(){}
 
+    TransferException(String message, Throwable cause){
+        super(message, cause);
     }
+
+    TransferException(Throwable cause){
+        super(cause);
+    }
+    
 }
